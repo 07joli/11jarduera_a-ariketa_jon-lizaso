20349855d8a31aedc62f78e0c0cd1701206ecf90
07joli <105489784+07joli@users.noreply.github.com>
2022-05-13 11:48:04 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(500)
+})
